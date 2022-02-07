47184fad211fc034720feb71ab6704ecaf454958
Aleksandr Vlasenko <a.yu.vlasenko0@gmail.com>
2022-02-07 19:41:42 +0300

    feat: change project structure to monorepo (nest + react)

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from "./app.module";
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
 
+    // todo: serve react client
+
     app.useGlobalPipes(new ValidationPipe({
         transform: true,
     }));
