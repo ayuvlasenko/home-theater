df89172bf35ec5d545737f1c8601647d6f6491e6
Aleksandr Vlasenko <a.yu.vlasenko0@gmail.com>
2022-03-31 17:18:25 +0300

    refactor: remove useless comment

--- apps/server/src/authentication/decorator/public.decorator.ts
+++ apps/server/src/authentication/decorator/public.decorator.ts
@@ -1,5 +1,4 @@
 import { SetMetadata } from "@nestjs/common";
 
 export const IS_PUBLIC_KEY = "isPublicRoute";
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
